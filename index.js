fca895f713450e0a84d8a9fc78bd2bce59dfe3fa
ajshemi <akpojotor@yahoo.com>
2020-03-20 18:18:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+const mapToNegativize=(arr) => {
+    const emptyArr=[]
+    arr.forEach(element => emptyArr.push(element*-1))
+    return emptyArr
+}
+
+const mapToNoChange=(arr) => {
+    const emptyArr=[]
+    arr.forEach(element => emptyArr.push(element))
+    return emptyArr
+}
+
+const mapToDouble=(arr) => {
+    const emptyArr=[]
+    arr.forEach(element => emptyArr.push(element*2))
+    return emptyArr
+}
+
+const mapToSquare=(arr) => {
+    const emptyArr=[]
+    arr.forEach(element => emptyArr.push(element**2))
+    return emptyArr
+}
+const reduceToTotal=(arr,start=0) => {
+    let sum=start
+    arr.forEach(element=> sum+=element )
+    return sum
+}
+
+const reduceToAllTrue=(arr) => {
+    return arr.every(element=>Boolean(element)===true)
+
+    
+}
+const reduceToAnyTrue=(arr) => {
+    return arr.some(element=>Boolean(element)===true)
+}
+// reduceToAllTrue(sourceArray)
+// reduceToAnyTrue(sourceArray)mapToSquare(sourceArray)
